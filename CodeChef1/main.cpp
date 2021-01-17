daff70bc423619ef582dcc738ee106d8661f3708
Vipin Kumar <vipinkumarkk1122@gmail.com>
2021-01-17 08:58:06 +0530

    Initial Commit

---
+++ CodeChef1/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+int tupleSameProduct(vector<int>& nums) {
+    
+    int count = 0;
+    
+    
+    for(long int i=0; i<nums.size(); i++) {
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+}
+
+
+int main()
+{
+    
+    
+    
+    
+    return 0;
+}
