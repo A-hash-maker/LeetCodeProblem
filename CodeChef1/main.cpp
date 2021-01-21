bb4393af8f777f06faed4569551e3ee5b7c0b4a8
Vipin Kumar <vipinkumarkk1122@gmail.com>
2021-01-21 20:40:00 +0530

    206. Reverse Linked List

--- CodeChef1/main.cpp
+++ CodeChef1/main.cpp
@@ -10,74 +10,58 @@
 
 using namespace std;
 
-bool isValid(string s) {
-    bool valied = false;
-    stack<char> paranthesis;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* reverseList(ListNode* head) {
+    ListNode* prev = NULL;
+    ListNode* current = head;
+    ListNode* next = NULL;
     
-    for(long int i=0; i<s.length(); i++) {
-        if(s[i] == '(' || s[i] == '[' || s[i] == '{') {
-            paranthesis.push(s[i]);
-        }else if(s[i] == ')') {
-            if(!paranthesis.empty()){
-                if(paranthesis.top() == '(') {
-                   paranthesis.pop();
-                }else {
-                    paranthesis.push(s[i]);
-                }
-            }else {
-                paranthesis.push(s[i]);
-            }
-        }else if(s[i] == ']') {
-            if(!paranthesis.empty()){
-                if(paranthesis.top() == '[') {
-                   paranthesis.pop();
-                }else {
-                    paranthesis.push(s[i]);
-                }
-            }else {
-                paranthesis.push(s[i]);
-            }
-        }else if(s[i] == '}') {
-            if(!paranthesis.empty()){
-                if(paranthesis.top() == '{') {
-                   paranthesis.pop();
-                }else {
-                    paranthesis.push(s[i]);
-                }
-            }else {
-                paranthesis.push(s[i]);
-            }
-        }
+    while(current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
     }
     
-    if(paranthesis.empty() && s.length() > 1) {
-        valied = true;
+    head = prev;
+    return head;
+}
+
+void push(ListNode** head_ref, int data) {
+    ListNode* new_node = new ListNode;
+    new_node->val = data;
+    new_node->next = *(head_ref);
+    *(head_ref) = new_node;
+}
+
+void printList(ListNode* head) {
+    while(head != NULL) {
+        cout << head->val << " ";
+        head = head->next;
     }
-    
-    
-    return valied;
+    cout << endl;
 }
 
 int main()
 {
-    string s = "[)]";
+    ListNode* head = NULL;
+    push(&head, 5);
+    push(&head, 4);
+    push(&head, 3);
+    push(&head, 2);
+    push(&head, 1);
     
-    isValid(s) ? cout << "Valied paranthesis" << endl: cout << "Not Valied paranthesis" << endl;
+    cout << "Original LinkList is " << endl;
+    printList(head);
     
-    s = "()[]{}";
-    
-    isValid(s) ? cout << "Valied paranthesis" << endl: cout << "Not Valied paranthesis" << endl;
-    
-    s = "(]";
-    
-    isValid(s) ? cout << "Valied paranthesis" << endl: cout << "Not Valied paranthesis" << endl;
-    s = "{[]}";
-    
-    isValid(s) ? cout << "Valied paranthesis" << endl: cout << "Not Valied paranthesis" << endl;
-    
-    
-    
-    
+    reverseList(head);
     
     return 0;
 }
