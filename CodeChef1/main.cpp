8f927ab07e8fcb3514bcccb6506e5f6e9850ad80
Vipin Kumar <vipinkumarkk1122@gmail.com>
2021-01-21 20:06:37 +0530

    January challenge Day 20 2021

--- CodeChef1/main.cpp
+++ CodeChef1/main.cpp
@@ -10,51 +10,74 @@
 
 using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
-
-void push(ListNode** root_ref, int data) {
-    ListNode* new_node = new ListNode;
-    new_node->val = data;
-    new_node->next = (*root_ref);
-    (*root_ref) = new_node;
-}
-
-int getDecimalValue(ListNode* head) {
-    int count = 0;
+bool isValid(string s) {
+    bool valied = false;
+    stack<char> paranthesis;
     
-    ListNode* headCount = head;
-    
-    while(headCount != NULL){
-        count++;
-        headCount = headCount->next;
-    }
-    count = count - 1;
-    long int sum = 0;
-    
-    while(head != NULL) {
-        if(head->val == 1) {
-            sum = sum + pow(2, count);
+    for(long int i=0; i<s.length(); i++) {
+        if(s[i] == '(' || s[i] == '[' || s[i] == '{') {
+            paranthesis.push(s[i]);
+        }else if(s[i] == ')') {
+            if(!paranthesis.empty()){
+                if(paranthesis.top() == '(') {
+                   paranthesis.pop();
+                }else {
+                    paranthesis.push(s[i]);
+                }
+            }else {
+                paranthesis.push(s[i]);
+            }
+        }else if(s[i] == ']') {
+            if(!paranthesis.empty()){
+                if(paranthesis.top() == '[') {
+                   paranthesis.pop();
+                }else {
+                    paranthesis.push(s[i]);
+                }
+            }else {
+                paranthesis.push(s[i]);
+            }
+        }else if(s[i] == '}') {
+            if(!paranthesis.empty()){
+                if(paranthesis.top() == '{') {
+                   paranthesis.pop();
+                }else {
+                    paranthesis.push(s[i]);
+                }
+            }else {
+                paranthesis.push(s[i]);
+            }
         }
-        head = head->next;
-        count--;
     }
     
-    return int(sum);
+    if(paranthesis.empty() && s.length() > 1) {
+        valied = true;
+    }
+    
+    
+    return valied;
 }
 
 int main()
 {
-    ListNode* head = NULL;
-    push(&head, 1);
+    string s = "[)]";
     
-    cout << getDecimalValue(head) << endl;
+    isValid(s) ? cout << "Valied paranthesis" << endl: cout << "Not Valied paranthesis" << endl;
+    
+    s = "()[]{}";
+    
+    isValid(s) ? cout << "Valied paranthesis" << endl: cout << "Not Valied paranthesis" << endl;
+    
+    s = "(]";
+    
+    isValid(s) ? cout << "Valied paranthesis" << endl: cout << "Not Valied paranthesis" << endl;
+    s = "{[]}";
+    
+    isValid(s) ? cout << "Valied paranthesis" << endl: cout << "Not Valied paranthesis" << endl;
+    
+    
+    
+    
     
     return 0;
 }
