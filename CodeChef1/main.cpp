6f8990a06f728083daebabc311bd38abbf4095f2
Vipin Kumar <vipinkumarkk1122@gmail.com>
2021-01-24 16:36:06 +0530

    1619. Mean of Array After Removing Some Elements

--- CodeChef1/main.cpp
+++ CodeChef1/main.cpp
@@ -11,30 +11,49 @@
 
 using namespace std;
 
-int countStudents(vector<int>& students, vector<int>& sandwiches) {
+double trimMean(vector<int>& arr) {
     
-    std::vector<int>::iterator it;
+    sort(arr.begin(), arr.end());
     
-    for(long int i=0; i<sandwiches.size(); i++) {
-        
-        it = std::find(students.begin(), students.end(), sandwiches[i]);
-        
-        if(it != students.end()) {
-            students.erase(students.begin() + (it - students.begin()));
-        }else {
-            break;
+    long int len = arr.size();
+    
+    long int len5 = len * 5;
+    len5 = len5 /100;
+    
+    //cout << "current value of len5 is " << len5 << endl;
+    
+    for(long int i=0; i<len5; i++) {
+        arr[i] = -1;
+        //cout << "Current Index of i is " << i << endl;
+    }
+    
+    for(long int i=len; i>(arr.size() - len5); i--) {
+        arr[i-1] = -1;
+        //cout << "Current Index of i is " << i << endl;
+    }
+    
+    double mean = 0.00000;
+    double countValue = 0;
+    
+    for(long int i=0; i<arr.size(); i++) {
+        if(arr[i] != -1) {
+            mean = mean + arr[i];
+            countValue++;
         }
     }
-    return int(students.size());
+    
+    mean = mean / countValue;
+    
+    
+    return mean;
 }
 
 
 int main()
 {
-    vector<int> students = {1, 1, 1, 0, 0, 1};
-    vector<int> sandwiches = {1, 0, 0, 0, 1, 1};
+    vector<int> arr = {6,2,7,5,1,2,0,3,10,2,5,0,5,5,0,8,7,6,8,0};
     
-    cout << countStudents(students, sandwiches) << endl;
+    cout << trimMean(arr) << endl;
     
     return 0;
 }
