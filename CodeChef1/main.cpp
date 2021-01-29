b91803f78480bb03587213e85973fad5ba8cc3e2
Vipin Kumar <vipinkumarkk1122@gmail.com>
2021-01-29 21:09:56 +0530

    922. Sort Array By Parity II

--- CodeChef1/main.cpp
+++ CodeChef1/main.cpp
@@ -14,13 +14,47 @@
 
 using namespace std;
 
-int search(vector<int>& nums, int target) {
+vector<int> sortArrayByParityII(vector<int>& A) {
     
-    for(long int i=0; i<nums.size(); i++) {
-        if(nums[i] == target) {
-            return i;
+    vector<int> oddNumbers;
+    vector<int> evenNumbers;
+    
+    for(long int i=0; i<A.size(); i++) {
+        if(A[i] % 2 == 0) {
+            evenNumbers.push_back(A[i]);
+        }else {
+            oddNumbers.push_back(A[i]);
         }
     }
     
-    return -1;
+    long int index1 = 0;
+    long int index2 = 0;
+    
+    for(long int i=0; i<A.size(); i++) {
+        if(i % 2 == 0) {
+            A[i] = evenNumbers[index1];
+            index1++;
+        }else {
+            A[i] = oddNumbers[index2];
+            index2++;
+        }
+    }
+    
+    cout << "required Array is as follow" << endl;
+    
+    for(long int i=0; i<A.size(); i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+    
+    return A;
+}
+
+int main()
+{
+    vector<int> A = {4, 2, 5, 7};
+    
+    sortArrayByParityII(A);
+    
+    return 0;
 }
