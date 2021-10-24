a7b4551258d436a383232b550c28c30fed966997
A Hash Maker <vipinkumarkk1122@gmail.com>
2021-10-24 10:05:59 +0530

    Daily Challenge Day 24 Count Complete Tree Nodes

--- CodeChef1/main.cpp
+++ CodeChef1/main.cpp
@@ -11,42 +11,11 @@
 
 using namespace std;
 
-bool searchMatrix(vector<vector<int>>& matrix, int target) {
+int countNodes(TreeNode* root) {
     
-    long int index = 0;
-    
-    for(long int i=0; i<matrix.size(); i++) {
-        long int last = matrix[i].size();
-        if(matrix[i][0] <= target && matrix[i][last - 1] >= target) {
-            index = i;
-            break;
-        }
+    if(root == NULL) {
+        return 0;
+    }else {
+        return countNodes(root->left) + countNodes(root->right) + 1;
     }
-    
-    for(long int j=0; j<matrix[index].size(); j++) {
-        if(matrix[index][j] == target) {
-            return true;
-        }
-    }
-    
-    
-    return false;
-}
-
-
-int main()
-{
-    
-    vector<vector<int>> matrix = {
-        {1, 3, 5, 7},
-        {10, 11, 16, 20},
-        {23, 30, 34, 60}
-    };
-    
-    int target = 10;
-    
-    cout << "Is Element is present in matrix " << searchMatrix(matrix, target) << endl;
-    
-    
-    return 0;
 }
