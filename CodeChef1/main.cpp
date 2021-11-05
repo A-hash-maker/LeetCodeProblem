c875ecdfe293de84b89383ede687f964c8f4a922
A Hash Maker <vipinkumarkk1122@gmail.com>
2021-11-06 05:12:53 +0530

    441. Arranging Coins

--- CodeChef1/main.cpp
+++ CodeChef1/main.cpp
@@ -11,47 +11,30 @@
 
 using namespace std;
 
-vector<int> findDuplicates(vector<int>& nums) {
+int arrangeCoins(int n) {
     
-    vector<int> duplicate;
+    long int var = 1;
+    int starCounts = 0;
     
-    if(nums.size() > 0) {
-        sort(nums.begin(), nums.end());
-        long int counter = 0;
+    while(n > 0) {
         
-        for(long int i=1; i<nums.size(); i++) {
-            if(nums[i-1] == nums[i]) {
-                
-                if(duplicate.size() == 0) {
-                    duplicate.push_back(nums[i]);
-                }else {
-                    if(duplicate[counter] != nums[i]) {
-                        duplicate.push_back(nums[i]);
-                        counter += 1;
-                    }
-                }
-                
-                
-            }
+        if((n - var) >= 0) {
+            n -= var;
+            starCounts++;
+            var += 1;
+        }else {
+            break;
         }
     }
     
-    cout << "Final Array " << endl;
-    
-    for(long int i=0; i<duplicate.size(); i++) {
-        cout << duplicate[i] << " ";
-    }
-    cout << endl;
-    
-    return duplicate;
+    return starCounts;
 }
-
 
 int main()
 {
-    vector<int> nums = {4, 3, 2, 7, 8, 2, 3, 1};
+    int n = 5;
     
-    findDuplicates(nums);
+    cout << arrangeCoins(n) << endl;
     
     return 0;
 }
